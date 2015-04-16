b36a521cf2cf0a098bc689ad0f93f6cf13d5c1be
KostadinHamanov <koci.hamanov@gmail.com>
2015-04-16 21:23:20 +0300

    Fixing some mistakes on spawn method

--- dungeon.py
+++ dungeon.py
@@ -2,9 +2,22 @@ class Dungeon():
 
     def __init__(self, file_name):
         self.file_name = file_name
+        self.map = []
+
+        with open(file_name, "r") as opened:
+            lines_list = opened.read().splitlines()
+
+            for line in lines_list:
+                self.map.append(list(line))
 
     def print_map(self):
-        with open(str(self.file_name), "r") as dungeon_map:
-            for line in dungeon_map:
-                splitted_line = line.rstrip("\n")
-                print (splitted_line)
+        for line in self.map:
+            print (line)
+
+    def spawn(self, hero):
+        for row in range(0, len(self.map)):
+            for col in range(0, len(self.map[row])):
+                if self.map[row][col] == 'S':
+                    self.map[row][col] = 'H'
+
+
