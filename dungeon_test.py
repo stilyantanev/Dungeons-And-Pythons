ebdf4651aaee386569bbdd99d407ac2a52a106fb
KostadinHamanov <koci.hamanov@gmail.com>
2015-04-16 21:38:32 +0300

    Adding spawn tests

--- dungeon_test.py
+++ dungeon_test.py
@@ -1,4 +1,5 @@
 from dungeon import Dungeon
+from hero import Hero
 import unittest
 
 
@@ -6,12 +7,22 @@ class TestDungeon(unittest.TestCase):
 
     def setUp(self):
         self.map_level1 = Dungeon('level1.txt')
+        self.hunter = Hero("Hunta", "Arrow", 120, 120, 2)
 
     def test_init(self):
         self.assertTrue(isinstance(self.map_level1, Dungeon))
 
-    def test_print_correct_map(self):
-        pass
+    def test_spawn(self):
+        expected_map_after_spawning = [
+            ['H', '.', '#', '#', '.', '.', '.', '.', '.', 'T'],
+            ['#', 'T', '#', '#', '.', '.', '#', '#', '#', '.'],
+            ['#', '.', '#', '#', '#', 'E', '#', '#', '#', 'E'],
+            ['#', '.', 'E', '.', '.', '.', '#', '#', '#', '.'],
+            ['#', '#', '#', 'T', '#', '#', '#', '#', '#', 'G']
+        ]
+        self.map_level1.spawn(self.hunter)
+        self.assertEqual(self.map_level1.map, expected_map_after_spawning)
+
 
 if __name__ == '__main__':
     unittest.main()
