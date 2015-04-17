61607f3276f0660e6d719dfe0813a27c4f589922
KostadinHamanov <koci.hamanov@gmail.com>
2015-04-17 16:51:02 +0300

    Adding the beginnig of move_hero()

--- dungeon.py
+++ dungeon.py
@@ -19,5 +19,112 @@ class Dungeon():
             for col in range(0, len(self.map[row])):
                 if self.map[row][col] == 'S':
                     self.map[row][col] = 'H'
+                    return True
+        return False
 
+    def get_current_position(self):
+        position = {}
+        for row in range(0, len(self.map)):
+            for col in range(0, len(self.map[row])):
+                if self.map[row][col] == 'H':
+                    position[0] = row
+                    position[1] = col
 
+        return position
+
+    def move_hero(self, direction):
+        current_position = self.get_current_position()
+        row = current_position[0]
+        col = current_position[1]
+
+        if direction == "up":
+            if row - 1 > 0:
+
+                if self.map[row - 1][col] == '#':
+                    return False
+
+                elif self.map[row - 1][col] == '.':
+                    self.map[row][col] = '.'
+                    self.map[row - 1][col] = 'H'
+                    return True
+
+                elif self.map[row - 1][col] == 'T':
+                    print ("Found treasure!")
+                    self.map[row][col] = '.'
+                    self.map[row - 1][col] = 'H'
+                    return True
+
+                elif self.map[row - 1][col] == 'E':
+                    pass
+
+            else:
+                return False
+
+        if direction == "down":
+            if row + 1 < len(self.map):
+
+                if self.map[row + 1][col] == '#':
+                    return False
+
+                elif self.map[row + 1][col] == '.':
+                    self.map[row][col] = '.'
+                    self.map[row + 1][col] = 'H'
+                    return True
+
+                elif self.map[row + 1][col] == 'T':
+                    print ("Found treasure!")
+                    self.map[row][col] = '.'
+                    self.map[row + 1][col] = 'H'
+                    return True
+
+                elif self.map[row + 1][col] == 'E':
+                    pass
+
+            else:
+                return False
+
+        if direction == "left":
+            if col - 1 > 0:
+
+                if self.map[row][col - 1] == '#':
+                    return False
+
+                elif self.map[row][col - 1] == '.':
+                    self.map[row][col] = '.'
+                    self.map[row][col - 1] = 'H'
+                    return True
+
+                elif self.map[row][col - 1] == 'T':
+                    print ("Found treasure!")
+                    self.map[row][col] = '.'
+                    self.map[row][col - 1] = 'H'
+                    return True
+
+                elif self.map[row][col - 1] == 'E':
+                    pass
+
+            else:
+                return False
+
+        if direction == "right":
+            if col + 1 < len(self.map[row]):
+
+                if self.map[row][col + 1] == '#':
+                    return False
+
+                elif self.map[row][col + 1] == '.':
+                    self.map[row][col] = '.'
+                    self.map[row][col + 1] = 'H'
+                    return True
+
+                elif self.map[row][col + 1] == 'T':
+                    print ("Found treasure!")
+                    self.map[row][col] = '.'
+                    self.map[row][col + 1] = 'H'
+                    return True
+
+                elif self.map[row][col + 1] == 'E':
+                    pass
+
+            else:
+                return False
